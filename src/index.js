feef1ed36f8b230a8ad65011b8cf6688bec51352
unknown <arteg132579@gmail.com>
2020-03-18 14:54:20 +0300

    feat: implement decoder function

--- src/index.js
+++ src/index.js
@@ -38,7 +38,50 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arrOfSymbols = [],
+        beggining = 0,
+        ending = 10,
+        splitedArrayOfSymbols = [],
+        arr3 = [],
+        arr,
+        morseSymbolsArray = [],
+        resultArray = [];
+    function splitting(expr) {
+        arrOfSymbols.push(expr.slice(beggining, ending));
+        if (ending !== expr.length) {
+            beggining = beggining + 10;
+            ending = ending + 10;
+            splitting(expr);
+        }
+    }
+    splitting(expr);
+    arrOfSymbols.forEach((item) => {
+        splitedArrayOfSymbols.push(item.split(''));
+    });
+    splitedArrayOfSymbols.forEach((item, i) => {
+        arr = item;
+        arr.forEach((item, i) => {
+            if ((item == 1) && (arr[i+1] == 0)) {
+                arr3.push('.');
+            } else if ((item == 1) && (arr[i+1] == 1)) {
+                arr3.push('-');
+                arr[i+1] = 0;
+            } else if (item == '*') {
+                arr3.push(' ');
+                arr.splice(i, 10);
+            }
+        });
+        morseSymbolsArray.push(arr3.join(''));
+        arr3 = [];
+    });
+    morseSymbolsArray.forEach((item, i) => {
+        if (item == ' ') {
+            resultArray.push(' ');
+        } else {
+            resultArray.push(MORSE_TABLE[item]);
+        }
+    });
+    return resultArray.join('');
 }
 
 module.exports = {
